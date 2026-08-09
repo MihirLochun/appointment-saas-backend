3ddc73ec938555ee7167a6f5c59c3a3ec2b7a6ae
Mihir Lochun <mihirlochun@gmail.com>
2026-08-09 11:54:38 +0400

    Add SSL configuration for Render database connections

--- db.js
+++ db.js
@@ -7,4 +7,7 @@ const { Pool } = pg;
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL.includes('sslmode=require')
+    ? { rejectUnauthorized: false }
+    : false,
 });
